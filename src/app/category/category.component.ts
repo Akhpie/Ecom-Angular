def18dee29c2a6e55b577bf1e90d20165a336ee0
Akhpie <134225827+Akhpie@users.noreply.github.com>
2024-08-02 22:01:17 +0530

    new changes

---
+++ src/app/category/category.component.ts
@@ -0,0 +1,11 @@
+import { Component, AfterViewInit } from '@angular/core';
+import Swiper from 'swiper';
+
+@Component({
+  selector: 'app-category',
+  templateUrl: './category.component.html',
+  styleUrl: './category.component.scss'
+})
+export class CategoryComponent {
+
+}
